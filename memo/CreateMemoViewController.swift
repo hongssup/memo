4eacdec24ac7d72b5be7d256dbc93d20b0fff7cd
seoyeon <hongssup0202@gmail.com>
2021-11-15 17:58:09 +0900

    Add SFSpeechRecognizerDelegate

--- memo/CreateMemoViewController.swift
+++ memo/CreateMemoViewController.swift
@@ -6,13 +6,20 @@
 //
 
 import UIKit
+import Speech
 
-class CreateMemoViewController: UIViewController {
+class CreateMemoViewController: UIViewController, SFSpeechRecognizerDelegate {
 
     @IBOutlet weak var textView: UITextView!
     
+    let speechRecognizer = SFSpeechRecognizer(locale: .init(identifier: "ko-KR"))
+    var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    var recognitionTask: SFSpeechRecognitionTask?
+    let audioEngine = AVAudioEngine()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        speechRecognizer?.delegate = self
         let toolbar = UIToolbar()
         let doneBtn = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(speechToText))
         let STT = UIBarButtonItem(title: "STT", style: .plain, target: self, action: #selector(speechToText))
@@ -25,6 +32,72 @@ class CreateMemoViewController: UIViewController {
     }
     
     @objc func speechToText() {
+        if audioEngine.isRunning {
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+            
+        } else {
+            startRecording()
+        }
+    }
+    
+    func startRecording() {
+        if recognitionTask != nil {
+            recognitionTask?.cancel()
+            recognitionTask = nil
+        }
         
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSession.Category.record)
+            try audioSession.setMode(AVAudioSession.Mode.measurement)
+            try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+        } catch {
+            print("audioSession properties weren't set because of an error.")
+        }
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        let inputNode = audioEngine.inputNode
+        
+        guard let recognitionRequest = recognitionRequest else {
+            print("Unable to create an SFSpeechAudioBufferRecognitionRequest object")
+            return
+        }
+        
+        recognitionRequest.shouldReportPartialResults = true
+        
+        recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
+            var isFinal = false
+            if result != nil {
+                self.textView.text = result?.bestTranscription.formattedString
+                isFinal = (result?.isFinal)!
+            }
+            
+            if error != nil || isFinal {
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+            }
+        })
+        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
+            self.recognitionRequest?.append(buffer)
+        }
+        audioEngine.prepare()
+        do {
+            try audioEngine.start()
+        } catch {
+            print("audioEngine couldn't start because of an error.")
+        }
+        //textView.text =
+    }
+    
+    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            
+        } else {
+            
+        }
     }
 }
